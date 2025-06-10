f4b2cccab2649dc6dd9dfea752859a4c82b6b8c2
7mochi <flyingcatdm@gmail.com>
2025-06-10 12:57:48 -0500

    chore: remove unused processor code

--- src/processor/processor.module.ts
+++ src/processor/processor.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProcessorService } from './processor.service';
-import { ProcessorController } from './processor.controller';
 
 @Module({
   imports: [],
   providers: [ProcessorService],
   exports: [ProcessorService],
-  controllers: [ProcessorController],
+  controllers: [],
 })
 export class ProcessorModule {}
